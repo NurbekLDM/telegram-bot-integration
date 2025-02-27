3c5fecaaf862424f44ad9718d4e91fff05fbea36
Aliqo'ziyev Nurbek <jumaboyevabarno80@gmail.com>
2025-02-27 18:09:02 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,19 +3,21 @@ const express = require('express');
 const app = express();
 
 const token = '7719771424:AAFRN7VsbLnEKZZ8av7htNeGvlwEJqHnSt8';
-const portfolioUrl = 'https://nurbek.codes'; // URL
+const portfolioUrl = 'https://nurbek.codes/';
 const bot = new TelegramBot(token);
 
 app.use(express.json());
 
 // Webhook yo'li
 app.post('/webhook', (req, res) => {
+  console.log('Webhook so‘rovi qabul qilindi:', req.body);
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
 // /start buyrug'i
 bot.onText(/\/start/, (msg) => {
+  console.log('Start buyrug‘i qabul qilindi:', msg);
   const chatId = msg.chat.id;
   const opts = {
     reply_markup: {
@@ -27,11 +29,4 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, 'Salom! Portfolio saytimni ochish uchun "Open" tugmasini bosing.', opts);
 });
 
-
-
-    bot.setWebHook('https://telegram-bot-integration.vercel.app//webhook');
-
-
-
 module.exports = app;
-
